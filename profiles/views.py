90a4996b642264e2cbf83ff1b191d86bcda24dee
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-04-20 19:30:01 +0000

    Annotating Profile views' querysets

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -7,11 +8,24 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListAPIView):
     """
     List all profiles.
-    No Create view as profile creation handled by django signals.
+    No Create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
@@ -19,4 +33,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
